9a8a50ad32a3ea61b3e56c2475b014eb410a000e
Emily Richter <emrichter1996@gmail.com>
2020-03-22 23:58:46 -0500

    Changing files

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+/*
+===================================================
+; Title:  portfolio.js
+; Author: Emily Richter
+; Date:   20 March 2020
+; Description: Required JavaScript file for portfolio. 
+; Will add more when larger JS file is needed.
+;==================================================
+*/
+
+// I'm having trouble knowing what to put for JavaScript,
+// but since it is required I have sent this blank file in.
